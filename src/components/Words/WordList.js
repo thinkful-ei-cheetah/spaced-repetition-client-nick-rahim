71d17989350542f0517d7c8a25d6f2fe219a66f5
Nick Lee <leexnick@gmail.com>
2019-06-25 10:45:18 -0700

    update WordList styles and update tests to match implementation

--- src/components/Words/WordList.js
+++ src/components/Words/WordList.js
@@ -9,26 +9,29 @@ class WordList extends Component {
   }
   render() {
     return (
-      <div className='WordList'>
-        <ul className='words'>
-          <h3 className='WordList_header'>Words</h3>
-          {this.props.words.map(word => (
-            <Words key={word.id} word={word.original} />
-          ))}{' '}
-        </ul>
-        <ul className='correct'>
-          <h3 className='WordList_header'>Correct</h3>
-          {this.props.words.map(word => (
-            <Words key={word.id} word={word.correct_count} />
-          ))}{' '}
-        </ul>
-        <ul className='incorrect'>
-          <h3 className='WordList_header'>Incorrect</h3>
-          {this.props.words.map(word => (
-            <Words key={word.id} word={word.incorrect_count} />
-          ))}{' '}
-        </ul>
-      </div>
+      <>
+        <h3 className='WordList_title'>Words to practice</h3>
+        <div className='WordList'>
+          <ul className='words'>
+            <h4 className='WordList_header'>Words</h4>
+            {this.props.words.map(word => (
+              <Words key={word.id} word={word.original} />
+            ))}{' '}
+          </ul>
+          <ul className='correct'>
+            <h4 className='WordList_header'>Correct</h4>
+            {this.props.words.map(word => (
+              <Words key={word.id} word={word.correct_count} />
+            ))}{' '}
+          </ul>
+          <ul className='incorrect'>
+            <h4 className='WordList_header'>Incorrect</h4>
+            {this.props.words.map(word => (
+              <Words key={word.id} word={word.incorrect_count} />
+            ))}{' '}
+          </ul>
+        </div>
+      </>
     );
   }
 }
