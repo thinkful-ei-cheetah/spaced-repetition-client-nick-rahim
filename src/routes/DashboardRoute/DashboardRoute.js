16b4818c9aa7e77f897af001231d7c7633d5cd2f
rahmor <rahmor@yahoo.com>
2019-06-25 11:42:48 -0400

    add language api

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import LanguageApiService from '../../services/language-api-service';
+import LanguageContext from '../../contexts/LanguageContext';
 
 class DashboardRoute extends Component {
+  static contextType = LanguageContext;
+
+  componentDidMount() {
+    LanguageApiService.getLanguage().then(response => {
+      const { language, words } = response;
+      this.context.setLanguage(language);
+      this.context.setWords(words);
+      this.context.setTotalScore();
+      console.log(this.context.language);
+      console.log(this.context.words);
+      console.log(this.context.words[0].original);
+    });
+  }
   render() {
     return (
       <section>
-        implement and style me
+        {/* {this.context.language} */}
+        {this.context.totalScore}
+        {/* {this.context.words[0].original} */}
       </section>
     );
   }
 }
 
-export default DashboardRoute
+export default DashboardRoute;
