cbc17498e171a646bf7f139195b4351196b03094
Nick Lee <leexnick@gmail.com>
2019-06-28 09:41:11 -0700

    fix typo in config for api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://pacific-hollows-60774.herokuapp.com//api',
+  API_ENDPOINT: 'https://pacific-hollows-60774.herokuapp.com/api',
   TOKEN_KEY: 'spaced-repetition-client-auth-token'
 }
+ 
