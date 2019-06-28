c30cc51bac2d2827a543dc767526c41102e46bd0
Nick Lee <leexnick@gmail.com>
2019-06-28 09:19:28 -0700

    add setups for deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'spaced-repetition-client-auth-token',
+  API_ENDPOINT: 'https://pacific-hollows-60774.herokuapp.com//api',
+  TOKEN_KEY: 'spaced-repetition-client-auth-token'
 }
